b226e617f4f16c53de5b28d7a93b335d01292ce8
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-03-13 21:58:19 +0000

    Commit before changing buttons

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ let inputNumber = 1;
 
 let limitNumber = 1;
 
+let clickNumber = 0;
+
+
 let inputValue = document.getElementById('inc-and-dec-input');
 
 let inputValueButton = document.getElementById('inc-and-dec-button');
@@ -26,32 +29,63 @@ const numberTag = document.getElementById('number');
 
 const list = document.getElementById('boom-list');
 
+const isEmpty = (id) => {
+  return document.getElementById(id).innerHTML.trim() == ''
+}
 
 const onIncrementClick = () => {
   number = number * inputNumber;
   numberTag.textContent = number;
-  if (number > (inputNumber / (inputNumber ** limitNumber)) && number <= 1)
-    while (list.firstChild) {
-      list.removeChild(list.firstChild)
-    }
-  if (number === (inputNumber ** limitNumber)) {
+  ++clickNumber;
+  if ((clickNumber === limitNumber || clickNumber === -Math.abs(limitNumber)) && isEmpty('boom-list')) {
     const boomItem = document.createElement('li');
     boomItem.textContent = '!bOoMm!';
     list.appendChild(boomItem);
+  } else if (clickNumber < limitNumber && clickNumber > -Math.abs(limitNumber)) {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
   }
 }
 
+// const onIncrementClick = () => {
+//   number = number * inputNumber;
+//   numberTag.textContent = number;
+//   if (number > (inputNumber / (inputNumber ** limitNumber)) && number <= 1)
+//     while (list.firstChild) {
+//       list.removeChild(list.firstChild)
+//     }
+//   if (number === (inputNumber ** limitNumber)) {
+//     const boomItem = document.createElement('li');
+//     boomItem.textContent = '!bOoMm!';
+//     list.appendChild(boomItem);
+//   }
+// }
 
 const onDecrementClick = () => {
   number = number / inputNumber;
   numberTag.textContent = number;
-  if (number < (inputNumber ** limitNumber) && number >= 1)
-    while (list.firstChild) {
-      list.removeChild(list.firstChild)
-    }
-  if (number === (inputNumber / (inputNumber ** limitNumber))) {
+  --clickNumber;
+  if ((clickNumber === limitNumber || clickNumber === -Math.abs(limitNumber)) && isEmpty('boom-list')) {
     const boomItem = document.createElement('li');
     boomItem.textContent = '!bOoMm!';
     list.appendChild(boomItem);
+  } else if (clickNumber < limitNumber && clickNumber > -Math.abs(limitNumber)) {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
   }
 }
+// const onDecrementClick = () => {
+//   number = number / inputNumber;
+//   numberTag.textContent = number;
+//   if (number < (inputNumber ** limitNumber) && number >= 1)
+//     while (list.firstChild) {
+//       list.removeChild(list.firstChild)
+//     }
+//   if (number === (inputNumber / (inputNumber ** limitNumber))) {
+//     const boomItem = document.createElement('li');
+//     boomItem.textContent = '!bOoMm!';
+//     list.appendChild(boomItem);
+//   }
+// }
